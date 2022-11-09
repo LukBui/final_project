83ab85354b274c525f42a01ccc2a067558105ee6
Lukas Buivydas <lukas.buivydas@gmail.com>
2022-11-09 19:11:52 +0200

    Creating Login and registration

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+
 
 app = Flask(__name__)
-
 bootstrap = Bootstrap(app)
 
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+ 
+@app.route('/signup')
+def login():
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
